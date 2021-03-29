d2b80bb7cee4c3cd92378eff45d9113b431d947c
ec965 <enoch965@gmail.com>
2021-03-29 16:15:57 -0700

    abstracting alert dialog

--- client/src/component/chat/messagelist/sidebuttons.tsx
+++ client/src/component/chat/messagelist/sidebuttons.tsx
@@ -5,20 +5,11 @@ import {
   useStyles,
   Tooltip,
   IconButton,
-  AlertDialog,
-  AlertDialogOverlay,
-  AlertDialogContent,
-  AlertDialogHeader,
-  AlertDialogBody,
-  AlertDialogFooter,
-  AlertDialogCloseButton,
-  Button,
   Heading,
   Text,
-  useMultiStyleConfig,
-
 } from '@chakra-ui/react';
 import { /*EllipsisVIcon, SmileIconRegular, ReplyIcon,*/ XIcon } from '../../icon';
+import { MyAlertDialog } from '../../alertdialog';
 
 interface SideButtonsProps {
   // onSmileClick?: React.MouseEventHandler<HTMLButtonElement>,
@@ -33,15 +24,10 @@ export const SideButtons: React.FC<SideButtonsProps> = ({
   onRemoveClick
 }) => {
   const [openDelete, setOpenDelete] = React.useState(false);
-  const cancelRef = React.useRef<any>();
 
   const iconStyles = useStyleConfig("Icon", { variant: "sideMsgButton" })
   const tooltipStyles = useStyleConfig("Tooltip", {variant: "sideMsgButton"})
   const parentStyles = useStyles();
-
-  const alertStyles = useMultiStyleConfig("AlertDialog", {variant: "removeMessage"});
-  const okayButtonStyle = useStyleConfig('Button', {variant: 'okay'});
-  const cancelButtonStyle = useStyleConfig('Button', {variant: 'cancel'});
 
   return (
     <Flex sx={parentStyles.sideButtons}>
@@ -62,43 +48,19 @@ export const SideButtons: React.FC<SideButtonsProps> = ({
           />
         </span>
       </Tooltip>
-      <AlertDialog
+      <MyAlertDialog
+        header="Would you like to remove this message?"
+        cancelButtonText="Cancel"
+        okayButtonText="Remove"
         isOpen={openDelete}
-        leastDestructiveRef={cancelRef}
-        onClose={(): void => setOpenDelete(false)}
-        isCentered
-        size="xl"
+        onClose={(): void => {setOpenDelete(false)}}
+        onOkayClick={onRemoveClick}
       >
-        <AlertDialogOverlay>
-          <AlertDialogContent>
-            <AlertDialogHeader sx={alertStyles.header}>
-              Would you like to remove this message?
-            </AlertDialogHeader>
-            <AlertDialogCloseButton sx={alertStyles.closeButton}/>
-            <AlertDialogBody>
-              <Heading fontSize="md">Unsend for everyone</Heading> 
-              <Text color="gray.500" fontSize="md">
-                You'll permanently remove this message for all chat members. They can see that you've unsent a message.
-              </Text>
-            </AlertDialogBody>
-            <AlertDialogFooter>
-              <Button 
-                ref={cancelRef}
-                onClick={(): void => setOpenDelete(false)}
-                sx={cancelButtonStyle}
-              >
-               Cancel
-              </Button>
-              <Button 
-                onClick={(e): void => {setOpenDelete(false); onRemoveClick(e);}}
-                sx={okayButtonStyle}
-              >
-                Remove
-              </Button>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialogOverlay>
-      </AlertDialog>
+        <Heading fontSize="md">Unsend for everyone</Heading> 
+        <Text color="gray.500" fontSize="md">
+          You'll permanently remove this message for all chat members. They can see that you've unsent a message.
+        </Text>
+      </MyAlertDialog>
       {/* <Tooltip
         label="React"
         placement="top"
